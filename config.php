8c0952e222a9bf9410e920528cd81a0958483019
Adam PM <adam.pm77@gmail.com>
2020-11-24 21:25:14 +0700

    last backup baik

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
 $connect_error = 'Sorry, we\'re experiencing connection issues.';
-$con = mysqli_connect('localhost', 'adam', '591990');
+$con = mysqli_connect('localhost', 'root', '');
 mysqli_select_db($con, 'h50505_lwldb') or die(mysqli_error($con));
