17ad7de597cc45e19e7a8361ff03074267241a21
Adam PM <adam.pm77@gmail.com>
2021-04-16 09:10:51 +0700

    update git ignore

--- config.php
+++
@@ -1,4 +0,0 @@
-<?php
-$connect_error = 'Sorry, we\'re experiencing connection issues.';
-$con = mysqli_connect('localhost', 'root', '');
-mysqli_select_db($con, 'h50505_lwldb') or die(mysqli_error($con));
