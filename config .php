6c3b78ffff783f5d7d5431bfc9eedbc881200e34
Adam PM <adam.pm77@gmail.com>
2025-10-15 10:58:19 +0700

    remove config.php and update menu.php for improved layout and structure

--- config .php
+++
@@ -1,5 +0,0 @@
-<?php
-$connect_error = 'Sorry, we\'re experiencing connection issues.';
-$con = mysqli_connect('localhost', 'manasama_lwl', 'admin!@#');
-mysqli_select_db($con, 'manasama_lwldb') or die(mysqli_error($con));
-?>
